9bb43d899053820f4c13763f2fd60b2e59e2f4af
mabutalid <theoaguilar123@gmail.com>
2022-10-07 01:03:21 +0800

    Header changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,32 @@ module.exports = {
 			roboto: ['Roboto Condensed', 'sans-serif'],
 			pacifico: ['Pacifico', 'cursive']
 		},
-		extend: {},
+		extend: {
+				animationDuration: {
+					00005: '0.5ms',
+					1000: '1000ms',
+					2000: '2000ms',
+					3000: '3000ms',
+					4000: '4000ms',
+					5000: '5000ms',
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: '0.1' },
+					'100%': { opacity: '1' },
+				},
+				slideIn: {
+					'0%': { transform: 'translateX(200%)' },
+					'100%': { transform: 'translateX(30%)' },
+				}
+			},
+			animation: {
+				slideIn: 'slideIn ease-in',
+				fadeIn: 'fadeIn ease-in',
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-animation')
+	],
 }
